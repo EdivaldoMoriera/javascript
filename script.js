b041d31121fb5d0578bebc3ea6a7ce4e275db96f
Edivaldo Moreira <37005158+EdivaldoMoriera@users.noreply.github.com>
2022-04-22 16:23:52 -0300

    acrescentado if e else

--- script.js
+++ script.js
@@ -5,5 +5,24 @@ function aluno(){
   n1 = Number(prompt('informe a nota do aluno')) 
   n2 = Number(prompt('informe a nota do aluno')) 
   media = (n1 +n2)/2
-  alert(` O aluno ${nome} ficou com as medias ${media} e foi aprovado`)
+  if(media<5){
+    alert(` O aluno ${nome} foi reprovado com a media ${media} `)
+  }
+  else if(media>5 && media<7){
+    alert(` O aluno ${nome} foi aprovado com a media ${media} 'REGULAR`)
+
+  }
+  else if(media>7 && media<8.5){
+    alert(` O aluno ${nome} foi aprovado com a media ${media} 'BOM`)
+
+  }
+  else if(media == 10){
+    alert(` O aluno ${nome} foi aprovado com a media ${media} 'ÓTIMO`)
+
+  }
+  else{
+    alert(` a media entre ${n1} e ${n2} é ${media} não pode ser aplicada para média escolar`)
+  }
+
+ 
 }
