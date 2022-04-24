3702625a1cea32b58ba5142182416dd27b20ea6f
Edivaldo Moreira <37005158+EdivaldoMoriera@users.noreply.github.com>
2022-04-24 07:49:08 -0300

    Update arrow_functon.js

--- arrow_function/arrow_functon.js
+++ arrow_function/arrow_functon.js
@@ -1,4 +1,12 @@
-let multiplicador = () =>{
+ /*let multiplicador = () =>{
     return 2*2+9
 }
 console.log(multiplicador())
+let multiplicador = (n, p) =>{
+    return n*p
+}
+console.log(multiplicador(2,3))*/
+let multiplicador = n =>{
+    return n*n
+}
+console.log(multiplicador(2))//arrow function
