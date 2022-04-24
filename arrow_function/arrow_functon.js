153b1f860807cec25d3b4448d5c108f48469bc5f
Edivaldo Moreira <37005158+EdivaldoMoriera@users.noreply.github.com>
2022-04-24 08:50:58 -0300

    testando a função par ou impar

--- arrow_function/arrow_functon.js
+++ arrow_function/arrow_functon.js
@@ -9,7 +9,7 @@ console.log(multiplicador(2,3))
 let multiplicador = n =>{
     return n*n
 }
-console.log(multiplicador(2))//arrow function*/
+console.log(multiplicador(2))//arrow function
 let parOuImpar = function(n){
     if(n%2 ==0){
 
@@ -19,4 +19,14 @@ else{
     return 'impar'
 }
 }
-console.log(parOuImpar(1))
+console.log(parOuImpar(1))*/
+function parOuImpar(n1){
+
+    if(n1%2==0){
+        return 'par'
+    }
+    else{
+        return 'impar'
+    }
+}
+console.log(parOuImpar(2))
